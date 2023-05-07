7e8105e09a647b97330cc10b3abf3cb93a5daf98
MDMahidul <mmbmahidul007@gmail.com>
2023-05-07 17:45:46 +0600

    added new loader function to load data

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Cart from '../Cart/Cart';
+import { useLoaderData } from 'react-router-dom';
 
 const Orders = () => {
+    const products = useLoaderData();
+    console.log(products);
+
+
+
     return (
         <div className='shop-container'>
             <div className='products-container'>
-                <h2>Orders Page</h2>
+                <h2>Orders Page: </h2>
             </div>
             <div className='cart-container'>
                 <Cart cart={[]}></Cart>
