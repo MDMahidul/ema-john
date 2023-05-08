7ef7d381ca656a1b9e24ea3a9b75a4abdf26b635
MDMahidul <mmbmahidul007@gmail.com>
2023-05-08 08:29:36 +0600

    added delete icon and show add cart product to the order review page

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import './Orders.css';
 
 const Orders = () => {
-    const products = useLoaderData();
-    console.log(products);
+    const cart = useLoaderData();
 
-
+    console.log(cart);
 
     return (
         <div className='shop-container'>
-            <div className='products-container'>
-                <h2>Orders Page: </h2>
+            <div className='review-container'>
+                {
+                    cart.map(product=><ReviewItem
+                    key={product.id}
+                    product={product}
+                    ></ReviewItem>)
+                }
             </div>
             <div className='cart-container'>
-                <Cart cart={[]}></Cart>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
