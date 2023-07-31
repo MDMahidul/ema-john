93d616b1d8fe4b6134578d88b645c8f4ba46d478
MDMahidul <mmbmahidul007@gmail.com>
2023-07-31 23:38:24 +0600

    Use Post To Load Some Products Using Keys to fixed pagination cart data showing problem

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -47,22 +47,35 @@ const Shop = () => {
 
   useEffect(() => {
     const storedCart = getShoppingCart();
-    const savedCart = [];
-    //step-1: get id of the addedProduct
-    for (const id in storedCart) {
-      //step-2: get product from products by using id
-      const addedProduct = products.find((product) => product._id === id);
-      if (addedProduct) {
-        //step-3: add quantity
-        const quantity = storedCart[id];
-        addedProduct.quantity = quantity;
-        //step-4: add the added product to the saved cart
-        savedCart.push(addedProduct);
+    const ids = Object.keys(storedCart);
+
+    fetch(`http://localhost:5000/productsByIds`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(ids),
+    })
+    .then(res=>res.json())
+    .then(cardProducts=>{
+      const savedCart = [];
+      //step-1: get id of the addedProduct
+      for (const id in storedCart) {
+        //step-2: get product from products by using id
+        const addedProduct = cardProducts.find((product) => product._id === id);
+        if (addedProduct) {
+          //step-3: add quantity
+          const quantity = storedCart[id];
+          addedProduct.quantity = quantity;
+          //step-4: add the added product to the saved cart
+          savedCart.push(addedProduct);
+        }
+        //step-5: set the cart
+        setCart(savedCart);
       }
-      //step-5: set the cart
-      setCart(savedCart);
-    }
-  }, [products]);
+    })
+
+  }, []);
 
   const handleAddToCart = (product) => {
     let newCart = [];
